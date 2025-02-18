062736397a24686c2ed09dd9c72473fbe4c5ca2c
Prafull <135959621+prafull-metta@users.noreply.github.com>
2025-02-18 17:12:03 +0530

    Update app.js
    Signed-off-by: Prafull <135959621+prafull-metta@users.noreply.github.com>

--- src/app.js
+++ src/app.js
@@ -53,7 +53,7 @@ const allowedOrigins = [
 
 app.use(
   cors({
-    origin: "https://data-collection-admin-mettasocial.vercel.app/*",
+    origin: allowedOrigins,
     credentials: true,
     methods: "GET,POST,PUT,DELETE,OPTIONS",
     allowedHeaders: "Content-Type,Authorization",
