680102e4eb327a4a6cdcf0d8e412e6da6f8baca4
Prafull <135959621+prafull-metta@users.noreply.github.com>
2025-02-18 17:08:16 +0530

    Update app.js
    Signed-off-by: Prafull <135959621+prafull-metta@users.noreply.github.com>

--- src/app.js
+++ src/app.js
@@ -53,7 +53,7 @@ const allowedOrigins = [
 
 app.use(
   cors({
-    origin: allowedOrigins,
+    origin: "*",
     credentials: true,
     methods: "GET,POST,PUT,DELETE,OPTIONS",
     allowedHeaders: "Content-Type,Authorization",
